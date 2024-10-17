15fb36e6c114261dd77ff63b7b2396a8b7cb4ff1
William Lempinen <wlempin@gmail.com>
2024-10-17 12:03:14 +0300

    removed env files, moved to vite.config

--- src/lib/api/apiClient.ts
+++ src/lib/api/apiClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import Cookies from 'js-cookie'
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_BASEURL,
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/json',
   },
