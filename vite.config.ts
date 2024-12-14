c79ba74706d6d47f9a53e199b007b1d492c864a2
William Lempinen <wlempin@gmail.com>
2024-12-14 17:25:56 +0200

    envs

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import * as dotenv from 'dotenv'
 
 export default defineConfig(({ mode }) => {
-  const envFilePath = `.env`
-  dotenv.config({ path: envFilePath })
-
   const isDev = mode === 'development'
-  const target = isDev ? 'http://localhost:8000' : process.env.VITE_API_URL
+  const target = isDev ? 'http://localhost:8000' : 'https://zatchat-api.azurewebsites.net/'
 
   if (!target) {
     throw new Error('VITE_API_URL is not defined in the environment file')
