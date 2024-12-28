52614e3cdae53687a46544e01c6e93743942784a
William Lempinen <wlempin@gmail.com>
2024-12-28 08:46:25 +0200

    app wrapper with suspense and error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,36 @@ import Home from './pages/Home'
 import Conversation from './pages/Conversation'
 import { ChatProvider } from './lib/webSocket/ChatContext'
 import { setupApiClient } from './lib/api/apiClient'
+import { ErrorBoundary } from 'react-error-boundary'
+import { Spinner } from './components/ui/Spinner'
+
+const ErrorFallback = () => {
+  return (
+    <div
+      className="flex h-screen w-screen flex-col items-center justify-center text-error"
+      role="alert"
+    >
+      <h2 className="text-lg font-bold">Ooops, something went wrong :( </h2>
+      <button className="mt-4" onClick={() => window.location.assign(window.location.origin)}>
+        Refresh
+      </button>
+    </div>
+  )
+}
+
+const Wrapper = ({ children }: PropsWithChildren) => {
+  return (
+    <React.Suspense
+      fallback={
+        <div className="flex h-screen w-screen items-center justify-center">
+          <Spinner size="xl" />
+        </div>
+      }
+    >
+      <ErrorBoundary FallbackComponent={ErrorFallback}>{children}</ErrorBoundary>
+    </React.Suspense>
+  )
+}
 
 const PrivateRoute = ({ children }: PropsWithChildren) => {
   const { isAuthenticated } = useAuth()
@@ -25,40 +55,42 @@ const App = () => {
   }, [logout])
 
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <PrivateRoute>
-                <Home />
-              </PrivateRoute>
-            }
-          />
-          <Route path="/login" element={<Access />} />
-          <Route path="/signup" element={<Access />} />
-          <Route
-            path="/home"
-            element={
-              <PrivateRoute>
-                <Home />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/conversation/"
-            element={
-              <PrivateRoute>
-                <ChatProvider>
-                  <Conversation />
-                </ChatProvider>
-              </PrivateRoute>
-            }
-          />
-        </Routes>
-      </Layout>
-    </Router>
+    <Wrapper>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <Home />
+                </PrivateRoute>
+              }
+            />
+            <Route path="/login" element={<Access />} />
+            <Route path="/signup" element={<Access />} />
+            <Route
+              path="/home"
+              element={
+                <PrivateRoute>
+                  <Home />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/conversation/"
+              element={
+                <PrivateRoute>
+                  <ChatProvider>
+                    <Conversation />
+                  </ChatProvider>
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </Layout>
+      </Router>
+    </Wrapper>
   )
 }
 
